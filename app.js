a5414128e6f8ab3638beb8c954b61317ba8495b8
Jorge-GodoyDuarte <hawkaa666@gmail.com>
2022-07-21 20:56:37 -0300

    agregando MVC

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3030;
+
+//recurso estaticos
+app.use(express.static('public'));
+//rutas
+app.get('/', (req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
+//servidor
+app.listen(port, () => console.log('Server running in http://localhost:' + port));
