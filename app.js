babea09d6bd8f874b57e37ee5dc441d937ee0c09
EmaDiaz01 <demanuel269@gmail.com>
2022-07-27 22:26:36 -0300

    probe nuevamente subir formularios

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require('express')
+const path = require('path')
+const app= express()
+const port = 3030
+
+app.listen(port,()=>console.log('Server running in port http://localhost:'+ port))
+
+
+app.use(express.static('public'))
+
+//app.get("/", (req, res)=>res.sendFile(path.join(__dirname,'views', 'login.html' )))
+
+app.get("/", (req, res)=>res.sendFile(path.join(__dirname,'views', 'register.html' )))
+
+
+app.get("/login", (req, res)=>res.sendFile(path.join(__dirname,'views', 'login.html' )))
