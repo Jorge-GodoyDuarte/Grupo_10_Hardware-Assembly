13c69a53bc8493acf7ceacab0ce092f5b6d4a009
juancruzmoran <juancruzmoran@outlook.es>
2022-07-29 20:47:57 -0300

    import in general

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.get('/', (req,res) => res.sendFile(path.join(__dirname,'views','home.html'))
 app.get('/shopping-cart', (req,res) => res.sendFile(path.join(__dirname,'views','shopping-cart.html')))
 app.get("/login", (req, res)=>res.sendFile(path.join(__dirname,'views', 'login.html' )))
 app.get("/register", (req, res)=>res.sendFile(path.join(__dirname,'views', 'register.html' )))
+app.get("/detail", (req, res)=>res.sendFile(path.join(__dirname,'views', 'detail.html' )))
 //servidor
 app.listen(port, () => console.log('Server running in http://localhost:' + port));
 
