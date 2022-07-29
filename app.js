25516a4b549446e67ca29b6c984d13fa65349ef5
juancruzmoran <juancruzmoran@outlook.es>
2022-07-29 19:28:16 -0300

    Merge antonio/develop_Juan

--- app.js
+++ app.js
@@ -13,16 +13,6 @@ app.get("/login", (req, res)=>res.sendFile(path.join(__dirname,'views', 'login.h
 app.get("/register", (req, res)=>res.sendFile(path.join(__dirname,'views', 'register.html' )))
 //servidor
 app.listen(port, () => console.log('Server running in http://localhost:' + port));
-const express=require('express')
-const path = require('path')
-const app= express()
-const port = 3030
-
-app.listen(port,()=>console.log('Server running in port http://localhost:'+ port))
-
-
-app.use(express.static('public'))
-
 
 
 
